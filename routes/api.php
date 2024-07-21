ca76c8d8f59dd4597fc4167637727c7de4e2c9dc
Xibarra11 <xandersibarra5@gmail.com>
2024-07-20 22:31:02 -0300

    funcion realizada para busqueda de personas

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,9 @@ Route::get('/personas', function(){
     return 'Este es el listado de las personas registradas';
 });
 
-
+Route::get('/personas/{id}', function(){
+    return 'Listado de personas por ID';
+});
 
 
 
